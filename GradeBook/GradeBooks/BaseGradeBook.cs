84b97083679c034cef7aefe01e2befcb99d6eace
Patryk Wiechec <patryk.wiechec@microsoft.wsei.edu.pl>
2022-03-19 13:41:12 +0100

    2/24

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -11,6 +11,8 @@ namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
     {
+
+        public GradeBookType Type { set; get; }
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
